5cc28c47318a8985a135237207deefa8c549272f
Kush Khatri <89057143+kushkhatri514@users.noreply.github.com>
2025-08-04 22:32:45 +0530

    Update upsideDownNumberTriangle.c

--- Pattern/Upside down Number right angled Triangle/upsideDownNumberTriangle.c
+++ Pattern/Upside down Number right angled Triangle/upsideDownNumberTriangle.c
@@ -2,16 +2,24 @@
 
 int main(void) {
     int n;
-    printf("enter the number of rows and coloumns =>");
-    scanf("%d",&n);
 
-    for(int i=1; i<=n; i++){
-        int a=1;
-        for(int j=1; j<=n+1-i; j++){
-            printf("%d ",a);
+    // Ask the user to input number of rows and columns
+    printf("Enter the number of rows and columns => ");
+    scanf("%d", &n);
+
+    // Outer loop for each row
+    for (int i = 1; i <= n; i++) {
+        int a = 1; // Start number from 1 for each row
+
+        // Inner loop to print decreasing number of columns
+        for (int j = 1; j <= n + 1 - i; j++) {
+            printf("%d ", a);
             a++;
         }
+
+        // Move to next line after each row
         printf("\n");
     }
+
     return 0;
 }
