ba2e063638293cb1457746e29e8322f27a4afcbd
Kush Khatri <89057143+kushkhatri514@users.noreply.github.com>
2025-08-11 19:27:30 +0530

    Update lucky_1676A.cpp

--- Problemset/Codeforces/Lucky?  rated 800  1676A/lucky_1676A.cpp
+++ Problemset/Codeforces/Lucky?  rated 800  1676A/lucky_1676A.cpp
@@ -4,25 +4,20 @@ using namespace std;
 int main(void) {
     int n; // Number of tickets
     cin >> n;
-
     long ticket;
-    
     for (int i = 1; i <= n; i++) {
         int back_sum = 0, front_sum = 0;
         cin >> ticket;
-        
         // Last 3 digits sum
         for (int k = 1; k <= 3; k++) {
             back_sum += ticket % 10;
             ticket /= 10;
         }
-        
         // First 3 digits sum
         for (int j = 1; j <= 3; j++) {
             front_sum += ticket % 10;
             ticket /= 10;
         }
-        
         if (back_sum == front_sum)
             cout << "yes" << endl;
         else
