d4fa3b28f6cc82945529b8a42e3ca5bae86570af
Kush Khatri <89057143+kushkhatri514@users.noreply.github.com>
2025-08-14 22:56:58 +0530

    Create Make_product_equal_one.cpp

---
+++ Problemset/Codeforces/Make Product Equal One - rated 900 - 1206B/Make_product_equal_one.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n; 
+    long long cost = 0;
+    int zerocount = 0;       // count of zero elements
+    int negetivecount = 0;   // count of negative elements
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        long long x;
+        cin >> x;
+        if (x > 0) {
+            cost = cost + x - 1; // reduce positive numbers to 1
+        }
+        else if (x < 0) {
+            cost = cost - x - 1; // increase negative numbers to -1
+            negetivecount++;
+        }
+        else {
+            cost = cost + 1; // convert 0 to 1
+            zerocount++;
+        }
+    }
+
+    // If odd count of negatives and no zero to fix it, convert one -1 to 1
+    if (negetivecount % 2 == 1) {
+        if (zerocount == 0) {
+            cost += 2;
+        }
+    }
+
+    cout << cost << endl;
+    return 0;
+}
+
